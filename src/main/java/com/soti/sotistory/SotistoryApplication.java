353b535059cba3a5d7e43170eb30c7e1ab35b727
Jalnik <silmusic0113@gmail.com>
2023-06-24 15:01:19 +0900

    first commit

---
+++ src/main/java/com/soti/sotistory/SotistoryApplication.java
@@ -0,0 +1,13 @@
+package com.soti.sotistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soti/sotistory/SotistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soti.sotistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

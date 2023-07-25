4feef2cb6c7e31baa287c5104a3dfb489ade8f3f
Rename52 <138089364+Rename52@users.noreply.github.com>
2023-07-25 17:06:25 +0900

    Update index.js

--- frontend-app/src/routes/intro/index.js
+++ frontend-app/src/routes/intro/index.js
@@ -1,47 +1,20 @@
 import gsap from 'gsap';
-import './intro.css';
 import Home from '../Home/Home';
 import { useState ,useEffect} from 'react';
+import VideoPlayer from '../../components/viewvideos';
 
 const Index = () => {
   const introTl = gsap.timeline();
   const [isIntroVisible, setIsIntroVisible] = useState(true);
   useEffect(() => {
-    function init() {
-      introTl.to('.loader__text span', { autoAlpha: 1 });
-      introTl.from('.loader__text span', {
-        yPercent: 150,
-        stagger: 0.125,
-        duration: 0.325,
-        ease: 'Power3.inOut'
-      });
-      introTl.to('.loader__text span', {
-        yPercent: 150,
-        stagger: 0.125,
-        duration: 0.325,
-        ease: 'Power3.inOut'
-      }, '+=.9');
-      introTl.to('.loader__slice', {
-        yPercent: 100,
-        stagger: 0.125,
-        duration: 0.6,
-        ease: 'Power3.inOut'
-      }, '<.125');
-      introTl.from('.hero__title span', {
-        yPercent: -100,
-        duration: 0.6,
-        ease: 'Power3.inOut'
-      }, '<.4.5');
-    }
 
-    init();
     
     const erase = setTimeout(() => {
       hideLoader();
     }, 2000);
     const timer = setTimeout(() => {
       handleButtonClick();
-    }, 4000);
+    }, 2000);
 
     return () => (clearTimeout(timer),clearTimeout(erase));
   }, []);
@@ -63,21 +36,11 @@ const Index = () => {
     <div>
       {isIntroVisible ? (
         <section id='loader' className="loader">
-          <span className="loader__text"><span>우석대학교</span></span>
-          <span className="loader__text"><span>정보보안학과</span></span>
-          <span className="loader__text"><span>동아리</span></span>
-          <div className="loader__slice"></div>
-          <div className="loader__slice"></div>
-          <div className="loader__slice"></div>
+          <VideoPlayer />
         </section>
       ) : (
         <Home />
       )}
-      {isIntroVisible ? (
-        <section className="hero">
-          <h1 className="hero__title"><span>Welcome, SOTI</span></h1>
-        </section>
-      ) : null}
     </div>
   );
 };

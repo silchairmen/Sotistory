7a397c4796cae1536136a93f3cccfd0f080025c5
Bobong <jhbgjy7@gmail.com>
2023-10-23 16:57:01 +0900

    조회 dto

---
+++ src/main/java/com/soti/sotistory/post/promotional/dto/PromotionalPostInfoDto.java
@@ -0,0 +1,26 @@
+package com.soti.sotistory.post.promotional.dto;
+
+import com.soti.sotistory.member.dto.MemberInfoProfileDto;
+import com.soti.sotistory.post.promotional.entity.PromotionalPost;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PromotionalPostInfoDto {
+
+    private Long postId; //POST의 ID
+    private String title;//제목
+    private String content;//내용
+    private String filePath;//업로드 파일 경로
+    private String writer;//작성자에 대한 정보
+
+    public PromotionalPostInfoDto(PromotionalPost promotionalPost) {
+        this.postId = promotionalPost.getId();
+        this.title = promotionalPost.getTitle();
+        this.content = promotionalPost.getContent();
+        this.filePath = promotionalPost.getFilePath();
+        this.writer = promotionalPost.getWriter().getNickname();
+    }
+
+}

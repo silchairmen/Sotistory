ce39094764beb43d58b4c4ce796d49a0609c9fd5
Bobong <jhbgjy7@gmail.com>
2023-10-23 10:52:48 +0900

    post 생성,수정시 date 자동 생성

---
+++ src/main/java/com/soti/sotistory/post/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.soti.sotistory.post;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedBy
+    private LocalDateTime lastModifiedDate;
+}

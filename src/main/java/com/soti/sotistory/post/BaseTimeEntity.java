aa25fb5b5c58dcb8d3f6178bac84e022ec94ed66
DevBO <42234678+BobongKu@users.noreply.github.com>
2023-10-24 01:07:18 +0900

    변경사항 없음

--- src/main/java/com/soti/sotistory/post/BaseTimeEntity.java
+++ src/main/java/com/soti/sotistory/post/BaseTimeEntity.java
@@ -2,7 +2,6 @@ package com.soti.sotistory.post;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 

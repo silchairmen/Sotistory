8d8386154dae405c9b0a02b034888a7de5c0a9b6
Bobong <jhbgjy7@gmail.com>
2023-10-24 16:05:00 +0900

    2023-10-24 Comment 로직 제거 나머지 기능 완료

--- src/main/java/com/soti/sotistory/post/exception/PostErrorCode.java
+++ src/main/java/com/soti/sotistory/post/exception/PostErrorCode.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum PostErrorCode {
 
     POST_NOT_FOUND(201,"글을 찾을 수 없습니다."),
-    NOT_AUTHORITY_UPDATE_POST(203, "글 수정 권한이 없습니다."),
-    NOT_AUTHORITY_DELETE_POST(203, "글 삭제 권한이 없습니다.");
+    NOT_AUTHORITY_UPDATE_POST(403, "글 수정 권한이 없습니다."),
+    NOT_AUTHORITY_DELETE_POST(503ㄴ, "글 삭제 권한이 없습니다.");
 
     private int status;
     private String message;

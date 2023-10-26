5513c5a85574fcb6d4313b80c90b7271fff620bd
Bobong <jhbgjy7@gmail.com>
2023-10-26 21:43:40 +0900

    프론트 분리

--- src/main/java/com/soti/sotistory/config/SecurityConfig.java
+++ src/main/java/com/soti/sotistory/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")
                 .deleteCookies();
 
-        http.cors().disable();
+        http.cors();
 
         //접근 권한 설정 관련
         http.authorizeRequests()

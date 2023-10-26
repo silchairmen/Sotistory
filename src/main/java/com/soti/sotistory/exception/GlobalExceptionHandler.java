7d351eaf1761f2dee7f1d6fa64c1901dc2fa357e
DevBO <42234678+BobongKu@users.noreply.github.com>
2023-10-27 00:01:44 +0900

    jar로 빌드 후 logging 시 exception log 안남는 문제 해결

--- src/main/java/com/soti/sotistory/exception/GlobalExceptionHandler.java
+++ src/main/java/com/soti/sotistory/exception/GlobalExceptionHandler.java
@@ -6,17 +6,28 @@ import com.soti.sotistory.post.exception.PostErrorResponse;
 import com.soti.sotistory.post.exception.PostException;
 import com.soti.sotistory.post.file.exception.FileErrorResponse;
 import com.soti.sotistory.post.file.exception.FileException;
+import com.soti.sotistory.utils.LogUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestControllerAdvice
+@AllArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final HttpServletRequest request;
 
     //post exception
     @ExceptionHandler({PostException.class})
     protected ResponseEntity postException(PostException e) {
         PostErrorResponse response = new PostErrorResponse(e.getErrorCode());
+        log.warn("postException({}) -> {}",response.getStatus(),response.getMessage());
         return ResponseEntity.ok().body(response);
     }
 
@@ -24,6 +35,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({FileException.class})
     protected ResponseEntity fileException(FileException e) {
         FileErrorResponse response = new FileErrorResponse(e.getErrorCode());
+        log.warn("fileException({}) -> {}",response.getStatus(),response.getMessage());
         return ResponseEntity.ok().body(response);
     }
 
@@ -31,6 +43,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({CommentException.class})
     protected ResponseEntity commentException(CommentException e) {
         CommentErrorResponse response = new CommentErrorResponse(e.getErrorCode());
+        log.warn("commentException({}) -> {}",response.getStatus(),response.getMessage());
         return ResponseEntity.ok().body(response);
+    }
+
+    //logging controllerAdvice를 선언한 class가 두개 일때 문제가 생길 수 있음
+    @ModelAttribute
+    public void logging(){
+        LogUtil.logInfo(request.getRemoteAddr() ,request.getRequestURI(), request.getMethod());
     }
 }

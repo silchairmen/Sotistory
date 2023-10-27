7d97fed53e4362df2af0c2bb1c68bbb10a61c573
orol7894 <aqsed147@gmail.com>
2023-10-27 13:44:48 +0900

    mypage conflict css fixed(hr tag)

--- frontend-app/src/routes/TestPage.js
+++ frontend-app/src/routes/TestPage.js
@@ -4,6 +4,7 @@ import './TestPage.scss';
 import photo from '../img/myphoto.jpg';
 import profile from '../img/profile_img_A.png';
 import MypageModal from './MypageModal';
+import styled from 'styled-components';
 const TestPage=()=> {
   const [nickname,setNickname] = useState("");
   const [name,setName] = useState("");
@@ -18,6 +19,14 @@ const TestPage=()=> {
   const [tistory,setTistory] = useState("");
   const [github,setGithub] = useState("");
   const [dreamhack,setDreamhack] = useState("");
+
+
+  const MypageHr = styled.hr`
+    width:100%;
+    margin-top:0px;
+    margin-bottom:0px;
+    margin-left:0px;
+  `
 
   const handleSave = async(event) => {
     event.preventDefault();
@@ -99,10 +108,10 @@ const TestPage=()=> {
       <div className='background'>
         <div className='menu__0'>
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'></link>
-        <div className="login">
-          <div className="login__content">
-            <div className="login__forms">
-              <form action="" className="login__register" id="login-up">
+        <div className="menu__mypage">
+          <div className="menu__mypage__content">
+            <div className="menu__mypage__forms">
+              <form action="" className="menu__mypage__register" id="menu__mypage-up">
                 <ul className='list'>
                   <li className='list__box'onClick={()=>handleMenuClick("0")}>
                     <img className='menu__profile' src={profile} alt='menu__profile'/>
@@ -116,37 +125,37 @@ const TestPage=()=> {
                     <img className='mypage_profile' src={photo} alt='mypage_profile'/>
                   </div>
                   <button className='img_button'>이미지 변경</button>
-                  <div className="login__inbox1">
-                    <p className='login__p'>닉네임</p>
-                    <hr/>
-                    <input type="text" id="nickname" className="login__input" value={nickname} onChange={handleNicknameChange}/>
+                  <div className="menu__mypage__inbox1">
+                    <p className='menu__mypage__p'>닉네임</p>
+                    <MypageHr/>
+                    <input type="text" id="nickname" className="menu__mypage__input" value={nickname} onChange={handleNicknameChange}/>
                   </div>
-            <div className="login__box">
-              <div className="login__inbox">
-              <p className='login__p'>이름</p>
-              <hr/>
-              <input type="text" id="name" className="login__input" value={name} onChange={handleNameChange}/>
+            <div className="menu__mypage__box">
+              <div className="menu__mypage__inbox">
+              <p className='menu__mypage__p'>이름</p>
+              <MypageHr/>
+              <input type="text" id="name" className="menu__mypage__input" value={name} onChange={handleNameChange}/>
               </div>
               </div>
-            <div className="login__box">
-              <div className='login__inbox'>
-              <p className='login__p'>기수</p>
-              <hr/>
-              <input type="text" id="joinyear" className="login__input" value={joinyear} onChange={handleJoinYearChange}/>
-              </div>
-            </div>
-            <div className="login__box">
-              <div className="login__inbox">
-              <p className='login__p'>학번</p>
-              <hr/>
-              <input type="text" id="stunum" className="login__input" value={stunum} onChange={handleStuNumChange}/>
+            <div className="menu__mypage__box">
+              <div className='menu__mypage__inbox'>
+              <p className='menu__mypage__p'>기수</p>
+              <MypageHr/>
+              <input type="text" id="joinyear" className="menu__mypage__input" value={joinyear} onChange={handleJoinYearChange}/>
               </div>
             </div>
-            <div className="login__box">
-              <div className="login__inbox">
-                <p className='login__p'>거주지</p>
-                <hr/>
-              <input type="text" id="address" className="login__input" value={address} onChange={handleAddressChange}/>
+            <div className="menu__mypage__box">
+              <div className="menu__mypage__inbox">
+              <p className='menu__mypage__p'>학번</p>
+              <MypageHr/>
+              <input type="text" id="stunum" className="menu__mypage__input" value={stunum} onChange={handleStuNumChange}/>
+              </div>
+            </div>
+            <div className="menu__mypage__box">
+              <div className="menu__mypage__inbox">
+                <p className='menu__mypage__p'>거주지</p>
+                <MypageHr/>
+              <input type="text" id="address" className="menu__mypage__input" value={address} onChange={handleAddressChange}/>
               </div>
             </div>
                 <MypageModal receiveData={receiveData}/>
@@ -161,10 +170,10 @@ const TestPage=()=> {
     return (
       <div className='background'>
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'></link>
-        <div className="login">
-          <div className="login__content">
-            <div className="login__forms">
-              <form action="" className="login__register" id="login-up">
+        <div className="menu__mypage">
+          <div className="menu__mypage__content">
+            <div className="menu__mypage__forms">
+              <form action="" className="menu__mypage__register" id="menu__mypage-up">
                 <ul className='list'>
                   <li className='list__box'onClick={()=>handleMenuClick("0")}>
                     <img className='menu__profile' src={profile} alt='profile_menu_img'/>
@@ -180,41 +189,41 @@ const TestPage=()=> {
                     <img className='mypage_profile' src={photo} alt='mypage_profile'/>
                   </div>
                   <button className='img_button'>이미지 변경</button>
-                  <div className="login__inbox1">
-                    <p className='login__p'>닉네임</p>
-                    <hr/>
-                    <input type="text" id="nickname" className="login__input" value={nickname} onChange={handleNicknameChange}/>
+                  <div className="menu__mypage__inbox1">
+                    <p className='menu__mypage__p'>닉네임</p>
+                    <MypageHr/>
+                    <input type="text" id="nickname" className="menu__mypage__input" value={nickname} onChange={handleNicknameChange}/>
                   </div>
-            <div className="login__box">
-              <div className="login__inbox">
-              <p className='login__p'>티스토리</p>
-              <hr/>
-              <input type="text" id="tistory" className="login__input" value={tistory} onChange={handleTistoryChange}/>
+            <div className="menu__mypage__box">
+              <div className="menu__mypage__inbox">
+              <p className='menu__mypage__p'>티스토리</p>
+              <MypageHr/>
+              <input type="text" id="tistory" className="menu__mypage__input" value={tistory} onChange={handleTistoryChange}/>
               </div>
               </div>
-            <div className="login__box">
-              <div className='login__inbox'>
-              <p className='login__p'>깃허브</p>
-              <hr/>
-              <input type="text" id="github" className="login__input" value={github} onChange={handleGithubChange}/>
-              </div>
-            </div>
-            <div className="login__box">
-              <div className="login__inbox">
-              <p className='login__p'>드림핵</p>
-              <hr/>
-              <input type="text" id="dreamhack" className="login__input" value={dreamhack} onChange={handleDreamhackChange}/>
+            <div className="menu__mypage__box">
+              <div className='menu__mypage__inbox'>
+              <p className='menu__mypage__p'>깃허브</p>
+              <MypageHr/>
+              <input type="text" id="github" className="menu__mypage__input" value={github} onChange={handleGithubChange}/>
               </div>
             </div>
-            <div className="login__box">
-              <div className="login__inbox">
-                <p className='login__p'>수상 \
+            <div className="menu__mypage__box">
+              <div className="menu__mypage__inbox">
+              <p className='menu__mypage__p'>드림핵</p>
+              <MypageHr/>
+              <input type="text" id="dreamhack" className="menu__mypage__input" value={dreamhack} onChange={handleDreamhackChange}/>
+              </div>
+            </div>
+            <div className="menu__mypage__box">
+              <div className="menu__mypage__inbox">
+                <p className='menu__mypage__p'>수상 \
                 이력</p>
-                <hr/>
-              <input type="text" id="address" className="login__input" value={address} onChange={handleAddressChange}/>
+                <MypageHr/>
+              <input type="text" id="address" className="menu__mypage__input" value={address} onChange={handleAddressChange}/>
               </div>
             </div>
-                <a className='login__button' onClick={handleSave}>
+                <a className='menu__mypage__button' onClick={handleSave}>
                   저장하기
                 </a>
               </form>

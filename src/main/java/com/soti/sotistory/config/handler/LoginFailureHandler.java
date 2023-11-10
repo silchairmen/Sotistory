0107d2464f49eca70e443f0bcfd093ded341475e
DevBO <jhbgjy7@gmail.com>
2023-11-10 18:10:10 +0900

    권한설정이 안되어있는 URL .authenticated() 자동 선언 되어 조치함

--- src/main/java/com/soti/sotistory/config/handler/LoginFailureHandler.java
+++ src/main/java/com/soti/sotistory/config/handler/LoginFailureHandler.java
@@ -30,7 +30,7 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
         Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("status", 200);
+        responseBody.put("status", 203);
         responseBody.put("message", "로그인 실패");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
